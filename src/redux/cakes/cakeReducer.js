8a68118837b015f3d105dbecfc8ab3a1517ea64d
Alens <alenskupnjak@yahoo.com>
2023-03-21 11:02:24 +0100

    Old solution, no hook

--- src/redux/cakes/cakeReducer.js
+++ src/redux/cakes/cakeReducer.js
@@ -1,20 +1,20 @@
-import {BUY_CAKE} from './cakeTypes'
+import { BUY_CAKE } from './cakeTypes';
 
 const initialState = {
-  numOfCakes:10
-}
+	numOfCakes: 10,
+};
 
+const cakeReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case BUY_CAKE:
+			return {
+				...state,
+				numOfCakes: state.numOfCakes - 1,
+			};
 
-const cakeReducer = (state = initialState, action ) => {
+		default:
+			return state;
+	}
+};
 
-  switch (action.type) {
-    case BUY_CAKE:
-      
-      break;
-  
-    default:
-      break;
-  }
-
-
-}
+export default cakeReducer;
